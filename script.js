e7dfd42e3aef3f135d4db8a99c2b22d563fdef09
Aleksandr Shiian <shiyanalex89@gmail.com>
2024-06-26 19:20:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('processButton').addEventListener('click', () => {
+document.getElementById('inputText').addEventListener('input', () => {
     const inputText = document.getElementById('inputText').value;
     const outputText = inputText.replace(/\n/g, '\n\n');
     document.getElementById('outputText').value = outputText;
@@ -9,5 +9,11 @@ document.getElementById('copyButton').addEventListener('click', () => {
     outputText.select();
     outputText.setSelectionRange(0, 99999); // For mobile devices
     document.execCommand('copy');
-    alert('Text copied to clipboard');
+
+    // Blink effect for the button
+    const copyButton = document.getElementById('copyButton');
+    copyButton.classList.add('blink');
+    setTimeout(() => {
+        copyButton.classList.remove('blink');
+    }, 400);
 });
